d4050fc3e8a47d3088c18bba1b964d56d7548cc0
kaustubhgit99 <kalpanashende1982@gmail.com>
2026-03-11 07:29:21 +0530

    Upload files from zip

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen bg-[#F7F7F7] flex items-center justify-center">
+      <div className="w-10 h-10 border-2 border-primary border-t-transparent rounded-full animate-spin"/>
+    </div>
+  )
+}
