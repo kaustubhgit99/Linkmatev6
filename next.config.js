ab2e4b15b3d50c2c99423f328b3bb8f341c5206d
kaustubhgit99 <kalpanashende1982@gmail.com>
2026-03-11 07:29:55 +0530

    Upload files from zip

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ndfqysbzwckegfrmrgan.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+}
+module.exports = nextConfig
